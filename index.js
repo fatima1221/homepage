6115cccc771de915f5317070e9f7b2ec1f151b00
Fatima Askerova <f.askerova@riberry.az>
2023-06-20 03:24:25 +0400

    added all my information and created a cool btn

---
+++ index.js
@@ -0,0 +1,7 @@
+function changeBoxShadow() {
+  const resume = document.querySelector(".resume");
+  resume.style.boxShadow = "1px 1px 7px 7px red";
+
+  const button = document.getElementById("tailButton");
+  button.innerHTML = "why did you do that? whyyyyy? 🙀";
+}
